f21e49f5114fa2d4749da160c9e4c4b8f580040d
hs00999 <hs00999@nate.com>
2018-01-05 18:28:40 +0900

    add update

--- app/src/main/java/com/example/jisungkim/app/Alone.java
+++ app/src/main/java/com/example/jisungkim/app/Alone.java
@@ -2,12 +2,135 @@ package com.example.jisungkim.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toast;
 
-public class Alone extends AppCompatActivity {
+import net.daum.mf.map.api.CameraUpdateFactory;
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapPointBounds;
+import net.daum.mf.map.api.MapView;
+
+public class Alone extends AppCompatActivity implements MapView.MapViewEventListener, MapView.POIItemEventListener {
+
+    private final int MENU_DEFAULT_CALLOUT_BALLOON = Menu.FIRST;
+    private final int MENU_CUSTOM_CALLOUT_BALLOON = Menu.FIRST + 1;
+
+    private static final MapPoint CUSTOM_MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.537229, 127.005515);
+    private static final MapPoint DEFAULT_MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.4020737, 127.1086766);
+
+    private MapView mMapView;
+    private MapPOIItem mDefaultMarker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_alone);
+        mMapView = (MapView) findViewById(R.id.map_view);
+        mMapView.setDaumMapApiKey(MapApiConst.DAUM_MAPS_ANDROID_APP_API_KEY);
+        mMapView.setMapViewEventListener(this);
+        mMapView.setPOIItemEventListener(this);
+
+        // 구현한 CalloutBalloonAdapter 등록
+        createDefaultMarker(mMapView);
+        showAll();
+    }
+
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, MENU_DEFAULT_CALLOUT_BALLOON, Menu.NONE, "Default CalloutBalloon");
+        menu.add(0, MENU_CUSTOM_CALLOUT_BALLOON, Menu.NONE, "Custom CalloutBalloon");
+
+        return true;
+    }
+
+    private void createDefaultMarker(MapView mapView) {
+        mDefaultMarker = new MapPOIItem();
+        String name = "Default Marker";
+        mDefaultMarker.setItemName(name);
+        mDefaultMarker.setTag(0);
+        mDefaultMarker.setMapPoint(DEFAULT_MARKER_POINT);
+        mDefaultMarker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+        mDefaultMarker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+
+        mapView.addPOIItem(mDefaultMarker);
+        mapView.selectPOIItem(mDefaultMarker, true);
+        mapView.setMapCenterPoint(DEFAULT_MARKER_POINT, false);
+    }
+
+    private void showAll() {
+        int padding = 20;
+        float minZoomLevel = 7;
+        float maxZoomLevel = 10;
+        MapPointBounds bounds = new MapPointBounds(CUSTOM_MARKER_POINT, DEFAULT_MARKER_POINT);
+        mMapView.moveCamera(CameraUpdateFactory.newMapPointBounds(bounds, padding, minZoomLevel, maxZoomLevel));
+    }
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
+
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem) {
+        Toast.makeText(this, "Clicked " + mapPOIItem.getItemName() + " Callout Balloon", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) {
+
+    }
+
+    @Override
+    public void onDraggablePOIItemMoved(MapView mapView, MapPOIItem mapPOIItem, MapPoint mapPoint) {
+
     }
 }
---
+++ app/src/main/java/com/example/jisungkim/app/MapApiConst.java
@@ -0,0 +1,9 @@
+package com.example.jisungkim.app;
+
+/**
+ * Created by pjh on 2014. 9. 15..
+ */
+public class MapApiConst {
+	// http://developers.daum.net/console
+    public static final String DAUM_MAPS_ANDROID_APP_API_KEY = "187add158e68c8c8df7144dac9d6950d";
+}
